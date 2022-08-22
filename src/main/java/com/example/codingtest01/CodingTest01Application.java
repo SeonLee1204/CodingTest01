0ec7e4bc38a5df61bea8e3286c5b23e34b4d967d
SeonLee1204 <81129200+SeonLee1204@users.noreply.github.com>
2022-08-23 01:55:08 +0900

    Initial commit

---
+++ src/main/java/com/example/codingtest01/CodingTest01Application.java
@@ -0,0 +1,13 @@
+package com.example.codingtest01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingTest01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodingTest01Application.class, args);
+    }
+
+}
